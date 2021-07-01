2029f336a6d18b6972a36147da46353676a0b9e6
Andrzej Franek <tehix1@gmail.com>
2021-07-01 17:55:50 +0200

    add javascript dropdown menu

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("hamburger").onclick = function () {
+    const menu = document.querySelector(".menu");
+    const overlay = document.querySelector(".navbar-overlay");
+    const burger = document.getElementById("hamburger");
+    if (!menu.classList.contains("display")) {
+        menu.classList.add("display");
+        overlay.classList.add("display");
+        burger.src = "./images/icon-hamburger.svg";
+    } else {
+        menu.classList.remove("display");
+        overlay.classList.remove("display");
+
+        burger.src = "./images/icon-close.svg";
+    }
+};
